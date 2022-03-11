55b03c109517faebf0f5a44d2bd25b9bb2939fbc
kulivers <kulivers@mail.ru>
2022-03-11 17:43:39 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import {useState} from "react";
+import TicTak from './TicTac'
 
 const App = () => {
-    const [, setText] = useState('');
-    const [updatedText, setUpdatedText] = useState('');
-    return (
+        return (
         <div className="App">
             <header className="App-header">
-                <div style={{maxWidth: 600}}>
-                    <textarea onChange={(e) => {
-                        setText(e.target.value)
-                        setUpdatedText(e.target.value.replaceAll('т.к.', 'так как'))
-                    }} style={{height: '200px', width: '600px', overflowWrap: 'break-word'}}/>
-
-                    <br/>
-                    <p style={{
-                        height: '300px', overflowWrap: 'break-word'
-                    }}>{updatedText}</p>
-                </div>
+             <TicTak/>
             </header>
         </div>
     );
